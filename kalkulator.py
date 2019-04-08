5b16fa0d2b506fdadac54dbbf66e03930485396f
Wiktor Zięcina <51170@student.ue.poznan.pl>
2019-04-08 17:35:00 +0200

    Funkcja HELP

--- kalkulator.py
+++ kalkulator.py
@@ -1,7 +1,14 @@
 print("To jest prosty kalkulator")
+
+def get_help():
+	print("Podaj dwie liczby:")
+
+
 def dodaj():
 	a = int(input())
 	b - int(input())
 	print(a+b)
+
 	
+get_help()
 print("Koniec programu!!!")
